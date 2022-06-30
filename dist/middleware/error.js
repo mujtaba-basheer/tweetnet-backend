2fa4ea63e14fe7badbb7c16373e4493752c012fe
Mujtaba Basheer <mujtaba.fleapo@gmail.com>
2022-06-30 20:38:35 +0530

    ...

--- dist/middleware/error.js
+++ dist/middleware/error.js
@@ -4,18 +4,17 @@ exports.errorHandler = exports.notFound = void 0;
 const app_error_1 = require("../utils/app-error");
 // for unspecified/unfound routes
 const notFound = (req, res, next) => {
-    return next(new app_error_1.default("Route does not exist.", 404));
+  return next(new app_error_1.default("Route does not exist.", 404));
 };
 exports.notFound = notFound;
 // error handler middleware
 const errorHandler = (err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    res.status(statusCode);
-    res.status(statusCode).json({
-        status: false,
-        message: err.message,
-        // adding error stack in development environment
-        stack: process.env.NODE_ENV === "production" ? null : err.stack,
-    });
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({
+    status: false,
+    message: err.message,
+    // adding error stack in development environment
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
 };
 exports.errorHandler = errorHandler;
